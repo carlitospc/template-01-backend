78c10f46f9010b5015192502e76474a1f6d2106c
Carlos Andrés Pupiales Córdoba <capc3101@gmail.com>
2024-12-16 17:51:42 -0500

    terminada version de template

--- src/main/java/com/emssanar/template/controller/CategoryRestController.java
+++ src/main/java/com/emssanar/template/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.emssanar.template.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.emssanar.template.model.Category;
 import com.emssanar.template.response.CategoryResponseRest;
 import com.emssanar.template.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
